32c45a3b89db2928e6fea1b192b08ccff89c0d83
Lukas Holzer <office@lukasholzer.com>
2018-01-10 13:53:06 +0100

    small update

--- handler.ts
+++ handler.ts
@@ -1,23 +1,33 @@
 import { Handler, Context, Callback } from 'aws-lambda';
-import fetch from 'node-fetch';
-import * as AWS from 'aws-sdk';
+// import fetch from 'node-fetch';
+// import * as htmlPdf from 'html-pdf-chrome'
+// import * as AWS from 'aws-sdk';
 
-const s3 = new AWS.S3();
+// const s3 = new AWS.S3();
+// const options: htmlPdf.CreateOptions = {
+//   port: 9222, // port Chrome is listening on
+// };
 
-// interface HelloResponse {
-//   statusCode: number;
-//   body: string;
-// }
+interface HelloResponse {
+  statusCode: number;
+  body: string;
+}
 
 const hello: Handler = (event: any, context: Context, callback: Callback) => {
-  // const response: HelloResponse = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: Math.floor(Math.random() * 10)
-  //   })
-  // };
+  const response: HelloResponse = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: Math.floor(Math.random() * 10)
+    })
+  };
 
-  // callback(undefined, response);
+  callback(undefined, response);
+
+  // const url = 'https://github.com/westy92/html-pdf-chrome';
+  // htmlPdf.create(url, options)
+  //   .then((pdf) => pdf.toBuffer());
+
+  /*
   fetch(event.image_url)
     .then((response: any) => {
       if (response.ok) {
@@ -37,6 +47,7 @@ const hello: Handler = (event: any, context: Context, callback: Callback) => {
       }).promise()
     ))
     .then((v: any) => callback(null, v), callback);
+    */
 };
 
 export { hello }
