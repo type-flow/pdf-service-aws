58539835f49a634c553b793cbad2b686fec41b18
Lukas Holzer <office@lukasholzer.com>
2018-02-05 12:34:31 +0100

    update

--- api/pdf.ts
+++ api/pdf.ts
@@ -1,60 +1,39 @@
-import Chromeless from "chromeless";
-
+import { Context, Callback } from 'aws-lambda';
+import Chromeless from 'chromeless'
 import { success, failure } from './libs/response.lib';
 
 
-export function main(event, context, callback) {
-    const c = new Chromeless();
+export async function main(event: any, context: Context, callback: Callback) {
+    const chromeless = new Chromeless({
+      remote: {
+        endpointUrl: 'https://neqq4t4bvf.execute-api.eu-west-1.amazonaws.com/dev/generate',
+        apiKey: 'jADd06KFfXkU8uDgiLRl2bdfIhx5ZuPUIEIMde00'
+      }
+    });
 
     try {
-      const place = c.goto(event.queryStringParameters.url)
-        .wait(event.queryStringParameters.selector || "body")
-        .screenshot(event.queryStringParameters.selector || "body");
+      const screenshot = await chromeless
+        .goto('https://www.google.com')
+        .type('chromeless', 'input[name="q"]')
+        .screenshot();
 
-      callback(null, success(place));
+      callback(null, success(screenshot));
     } catch( e ) {
       callback(null, failure({
         status: false,
         error: `Couldn\'t create screenshot`,
-        debug: { stackTrace: e, params: event.queryStringParameters }
+        debug: { stackTrace: e }
+      }));
+    }
+
+    try {
+      await chromeless.end()
+    } catch(e) {
+
+      callback(null, failure({
+        status: false,
+        error: `Couldn\'t stop Chromeless`,
+        debug: { stackTrace: e }
       }));
     }
 }
-
-
-// export async function generate(event, context, callback) {
-
-//   if(!event.options || !event.options.url) {
-//     console.error('No options provided for PDF creation');
-//     return callback(new Error('Couldn\'t create the todo item.'));
-//   }
- 
-
-//   // remote: {
-//   //   endpointUrl: 'https://XXXXXXXXXX.execute-api.eu-west-1.amazonaws.com/dev',
-//   //   apiKey: 'your-api-key-here',
-//   // };
-//   // const pdfFilename = `${event.options.filename}.${event.options.filetype}`;
-
-//   const chromeless = new Chromeless({ remote: true })
-//   const pdf = await chromeless
-//     .goto(event.options.url)
-//     .pdf({
-//       landscape: false,
-//       displayHeaderFooter: false,
-//       printBackground: false,
-//       scale: 1,
-//       paperWidth: 8.27,
-//       paperHeight: 11.69,
-//       marginTop: 0,
-//       marginBottom: 0,
-//       marginLeft: 0,
-//       marginRight: 0
-//   })
-
-//   console.log(pdf) // prints local file path or S3 URL
-
-//   await chromeless.end();
-
-//   callback(null, success({"pdf": pdf}));
-// };
